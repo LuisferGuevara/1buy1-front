ffd83f07b11704ecaeebf652b8c4b55e057d45c3
Karuh13 <alvaro.c.s.13@gmail.com>
2022-12-01 18:37:15 +0100

    FEAT: created AuthRoute component

---
+++ src/components/AuthRoute.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const AuthRoute = ({ component }) => {
+	const { user } = useSelector((state) => state.auth);
+	if (user === null) return <Navigate to="/login" />;
+	if (user) return component;
+};
+
+export default AuthRoute
