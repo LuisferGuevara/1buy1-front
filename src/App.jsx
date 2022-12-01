c0bb39f20ca77be3740b33a34ef7770f6d859cfd
diego <diegomiller7@gmail.com>
2022-12-01 19:08:17 +0100

    FEAT: adding header component and rough styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header";
 import { Comparator, Login, Register } from "./pages/index";
 
 
 function App() {
   return (
     <div className="app">
+    <Header></Header>
     <Routes>
       <Route path="/comparator" element={<Comparator/>} />
       <Route path="/register" element={<Register/>} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import "../styles/Header.scss";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/comparator'>Comparator</NavLink>       
+      </nav>
+      <div>
+        <img 
+        src='https://res.cloudinary.com/dfxn0bmo9/image/upload/v1669916552/icons/1buy1-logo1-03_glar7y.svg' 
+        alt='1buy1Logo'/>
+      </div>
+      <div className='header--user'>
+        <NavLink>
+        <img src='https://cdn-icons-png.flaticon.com/512/1946/1946429.png' alt='userIcon'/>
+        </NavLink>
+      </div>
+    </header>
   )
 }
 
