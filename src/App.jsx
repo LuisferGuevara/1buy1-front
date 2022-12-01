601315afbbccad6b3a8e7cff285dc593cee0243c
Karuh13 <alvaro.c.s.13@gmail.com>
2022-12-01 18:19:37 +0100

    FEAT: login page, added route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import { Comparator } from "./pages/index";
+import { Comparator, Login } from "./pages/index";
 
 function App() {
   return (
     <div className="app">
     <Routes>
       <Route path="/comparator" element={<Comparator/>} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
     </div>
   );
