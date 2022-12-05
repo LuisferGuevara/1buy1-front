64fb3292dd0cff836a5b09bb3dc0f02f1816796c
JimmyGx6 <jaime92_92@hotmail.com>
2022-12-05 01:24:35 +0100

    REFACTOR: restore to last situation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,40 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Product.scss";
 
 const Product = ({ product }) => {
-  const [detail, setDetail] = useState(true);
-  const { supermarkets } = product;
-
-  const clickHandler = (product) => {
-    setDetail(false);
-  };
-
   return (
-    <>
-      {detail ? (
-        <div className="food-item" onClick={() => clickHandler(product)}>
-          <div className="food-item__image">
-            <img src={product.img} alt={product.name} />
-          </div>
-          <h2>{product.name}</h2>
-        </div>
-      ) : (
-        <div>
-          {supermarkets.map((supermarket) => (
-            <div key={supermarket._id}>
-              <p> {supermarket.supermarketName} </p>
-              <div>
-                <img src={supermarket.logo} alt={supermarket.supermarketName} />
-              </div>
-              <p>{supermarket.productName}</p>
-              <p>{supermarket.priceUd} €</p>
-              {supermarket.priceKg && <p>{supermarket.priceKg} €/Kg</p>}
-              {supermarket.priceL && <p>{supermarket.priceL} €/L</p>}
-            </div>
-          ))}
-        </div>
-      )}
-    </>
+    <div className="food-item" >
+      <div className="food-item__image">
+        <img src={product.img} alt={product.name} />
+      </div>
+      <h2>{product.name}</h2>
+    </div>
   );
 };
 
