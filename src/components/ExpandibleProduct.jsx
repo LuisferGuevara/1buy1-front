ef90af40781e94df65d070eff64155a243ae7441
Karuh13 <alvaro.c.s.13@gmail.com>
2022-12-05 02:10:32 +0100

    FEAT: Product name change to ExpandibleProduct, applied logic for detail card

---
+++ src/components/ExpandibleProduct.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { motion, AnimateSharedLayout } from "framer-motion";
+
+function ExpandedCard({ product, onCollapse }) {
+	const { supermarkets } = product;
+
+	return (
+		<>
+			<motion.p
+				className="expanded secondary"
+				onClick={onCollapse}
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+			/>
+			<motion.div className="card expanded" layoutId="expandable-card">
+				<button onClick={onCollapse}>X</button>
+				{supermarkets.map((supermarket) => (
+					<div key={supermarket._id}>
+						<p> {supermarket.supermarketName} </p>
+						<div>
+							<img src={supermarket.logo} alt={supermarket.supermarketName} />
+						</div>
+						<p>{supermarket.productName}</p>
+						<p>{supermarket.priceUd} €</p>
+						{supermarket.priceKg && <p>{supermarket.priceKg} €/Kg</p>}
+						{supermarket.priceL && <p>{supermarket.priceL} €/L</p>}
+					</div>
+				))}
+			</motion.div>
+		</>
+	);
+}
+
+function CompactCard({ product, onExpand }) {
+  console.log(product);
+	return (
+		<motion.div className="food-item" layoutId="expandable-card" onClick={onExpand}>
+			<div className="food-item__image">
+				<img src={product.img} alt={product.name} />
+			</div>
+			<h2>{product.name}</h2>
+		</motion.div>
+	);
+}
+
+const ExpandibleProduct = ({product}) => {
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const collapseDate = () => {
+		setIsExpanded(false);
+	};
+
+	const expandDate = () => {
+		setIsExpanded(true);
+	};
+
+	return (
+		<div className="card-container">
+			<AnimateSharedLayout>
+				{isExpanded ? (
+					<ExpandedCard onCollapse={collapseDate} product={product} />
+				) : (
+					<CompactCard onExpand={expandDate} product={product} />
+				)}
+			</AnimateSharedLayout>
+		</div>
+	);
+};
+
+export default ExpandibleProduct;
